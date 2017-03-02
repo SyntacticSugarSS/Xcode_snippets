bcb5bde73e554f7409de16ab25a495a611ea9ba4
SyntacticSugarSS <1393290187@qq.com>
2017-03-02 17:47:52 +0800

    代码片段

---
+++ changeSnippets.sh
@@ -0,0 +1,4 @@
+
+mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+ln -s $(pwd)/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+echo "done"
